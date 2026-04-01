e69b6b84c5c5e3d049c3df676af422002d60a624
Robosword <32asingh@stratfordschools.net>
2026-04-01 09:04:34 -0700

    Update manageMent.java
    Read from the given file.

--- manageMent.java
+++ manageMent.java
@@ -1,4 +1,18 @@
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.File;
+
 public class manageMent {
-    Scanner scanny =  new Scanner("");
+    String fileStringy = "";
+    manageMent(){
+    try (Scanner fileScanner = new Scanner(new File("myfile.txt"))) { //
+        while (fileScanner.hasNextLine()) { //
+            String data = fileScanner.nextLine(); //
+            fileStringy += data;
+            fileStringy += "\n";
+        }
+    } catch (FileNotFoundException e) {
+        System.out.println("An error occurred: File not found.");
+        e.printStackTrace();
+    }}
 }
