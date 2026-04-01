60a8f65a2fbebe20a2e409b2d547290fb1398671
roboswordblog <32asingh@stratfordschools.net>
2026-03-31 20:45:45 -0700

    Update Main.java
    Make the window, set the size, make it visible.

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        JFrame window = new JFrame("Coding Ide");
+        window.setSize(800, 600);
+        window.setVisible(true);
     }
 }
