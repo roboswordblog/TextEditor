bb555b8def94a9a53625e4500e273333fc947cae
roboswordblog <32asingh@stratfordschools.net>
2026-03-31 21:08:58 -0700

    Update Main.java
    Added the openbutton.

--- Main.java
+++ Main.java
@@ -9,19 +9,28 @@ public class Main {
         window.setLocationRelativeTo(null);
         window.setResizable(false);
 
-
         window.setLayout(null);
 
         JButton saveButton = new JButton("Save");
-        saveButton.setBounds(10, 10, 100, 40); 
+        JButton openButton = new JButton("Load");
+        saveButton.setBounds(10, 10, 100, 40);
+        openButton.setBounds(110, 10, 100, 40);
         saveButton.setBackground(new Color(45, 45, 45));
+        openButton.setBackground(new Color(45, 45, 45));
         saveButton.setForeground(Color.WHITE);
         saveButton.setFocusPainted(false);
+        openButton.setForeground(Color.WHITE);
+        openButton.setFocusPainted(false);
         saveButton.setBorder(BorderFactory.createLineBorder(new Color(70, 70, 70)));
+        openButton.setBorder(BorderFactory.createLineBorder(new Color(70, 70, 70)));
         saveButton.setFont(new Font("Consolas", Font.BOLD, 14));
         saveButton.setOpaque(true);
         saveButton.setContentAreaFilled(true);
+        openButton.setFont(new Font("Consolas", Font.BOLD, 14));
+        openButton.setOpaque(true);
+        openButton.setContentAreaFilled(true);
 
+        window.add(openButton);
         window.add(saveButton);
 
 
