6bb3ba599a3e3212619e6dab7b3c6e31ec214526
Robosword <32asingh@stratfordschools.net>
2026-04-01 20:36:45 -0700

    Update manageMent.java

--- manageMent.java
+++ manageMent.java
@@ -1,20 +1,23 @@
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
 
 public class manageMent {
     String fileStringy = "";
     manageMent(){
-    try (Scanner fileScanner = new Scanner(new File("myfile.txt"))) { //
-        while (fileScanner.hasNextLine()) { //
-            String data = fileScanner.nextLine(); //
-            fileStringy += data;
-            fileStringy += "\n";
-        }
-    } catch (FileNotFoundException e) {
-        System.out.println("An error occurred: File not found.");
-        e.printStackTrace();
-    }}
+        try (Scanner fileScanner = new Scanner(new File("myfile.txt"))) { //
+            while (fileScanner.hasNextLine()) { //
+                String data = fileScanner.nextLine(); //
+                fileStringy += data;
+                fileStringy += "\n";
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred: File not found.");
+            e.printStackTrace();
+        }}
     void save(String content) throws IOException{
         Files.writeString(Path.of("a.txt"), content);
     }
